fe55d3f74f8e00fb201cd60af5396365754ec2ad
Charles Fox <charfox1@umbc.edu>
2020-11-09 22:41:11 -0500

    add test user to proper table

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -1,17 +1,32 @@
+// This file has all the functions for creating and interacting with the tables in mongodb
 
-async function listDatabases(client){
+const tables = ["Users", "Employee", "PersonInNeed", "Teams", "Events", "Missions"]
+
+async function listDatabases(client) {
     databasesList = await client.db().admin().listDatabases();
- 
+
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
+
+// check if each table exists and add it if not
+// don't need to do this.  Mongo automatically creates collections as needed
+
+async function addUser(client, name, pass) {
+    var collection = client.db("AFRMS").collection("Users")
+    var doc = {
+        name: name,
+        password: pass
+    }
+    collection.insertOne(doc)
+}
 
 async function main(){
 
     // attempt to connect to the database
     const MongoClient = require('mongodb').MongoClient;
     const uri = "mongodb+srv://Admin:Password@cluster0.ejcge.mongodb.net/AFRMS?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true });
+    const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true });
 
     try {
         // connect to the cluster
@@ -22,9 +37,9 @@ async function main(){
 
     } catch (e) {
         console.error(e);
-    } finally {
-        await client.close();
     }
+
+    addUser(client, "test", "test")
 
 }
 
