875c5b268f1bacbb4e036afaa0065a908eca107e
Charles Fox <charfox1@umbc.edu>
2020-11-12 19:22:12 -0500

    Disallow duplicate employees

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -13,70 +13,57 @@ async function listDatabases(client) {
 // don't need to do this.  Mongo automatically creates collections as needed
 
 async function addUser(client, name, pass, role) {
-    
-    try {
-        // connect to the cluster
-        await client.connect();
 
+    var collection = client.db("AFRMS").collection("Users");
+    var doc = {
+        name: name,
+        password: pass,
+        role: role
+    };
+    collection.insertOne(doc);
+    
+}
+
+async function addEmployee(client, name, pass, role, availability) {
+
+    console.log("Checking if employee already exists");
+    const query = {name: name, password: pass, role: role};
+    var exists = await client.db("AFRMS").collection("Users").findOne(query)
+    console.log(exists);
+    if (exists != null) {
+        console.log("This employee already exists!");
+        return;
+    }
+    
+    console.log("Checking if employee user already exists");
+    var exists = await client.db("AFRMS").collection("Users").findOne(query)
+    console.log(exists)
+    if (exists == null) {
+        console.log("User does not exist yet. adding it");
         var collection = client.db("AFRMS").collection("Users");
         var doc = {
             name: name,
             password: pass,
             role: role
         };
-        collection.insertOne(doc);
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-    
-}
-
-async function addEmployee(client, name, pass, role, availability) {
-        
-    console.log("adding employee");
-
-    try {
-        // connect to the cluster
-        await client.connect();
-
-        console.log("Checking if employee user already exists");
-        
-        const query = {name: name, password: pass, role: role};
-        var exists = await client.db("AFRMS").collection("Users").findOne(query)
-        console.log(exists)
-        if (exists == null) {
-            console.log("User does not exist yet. adding it");
-            var collection = client.db("AFRMS").collection("Users");
-            var doc = {
-                name: name,
-                password: pass,
-                role: role
-            };
-        collection.insertOne(doc);
-        }
-
-        const user = await client.db("AFRMS").collection("Users").findOne(query);
-        console.log(user);
-        var userID = user._id;
-        console.log(userID);
-
-        var collection = client.db("AFRMS").collection("Employee");
-        var doc = {
-            userID: userID,
-            availability: availability,
-        };
-        collection.insertOne(doc);
-        console.log("added employee");
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
+    collection.insertOne(doc);
     }
 
+    const user = await client.db("AFRMS").collection("Users").findOne(query);
+    console.log(user);
+    var userID = user._id;
+    console.log(userID);
+
+    var collection = client.db("AFRMS").collection("Employee");
+    var doc = {
+        userID: userID,
+        name: name,
+        password: pass,
+        role: role,
+        availability: availability,
+    };
+    collection.insertOne(doc);
+    console.log("added employee");
 }
 
 async function main() {
@@ -86,8 +73,18 @@ async function main() {
     const uri = "mongodb+srv://Admin:Password@cluster0.ejcge.mongodb.net/AFRMS?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
 
-    //addUser(client, "test", "test", "Cool person");
-    addEmployee(client, "test2", "test2", "Operations Chief", true);
+    try {
+        // connect to the cluster
+        await client.connect();
+
+        // await function calls
+        await addEmployee(client, "test2", "test2", "Operations Chief", true);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 
 }
 
